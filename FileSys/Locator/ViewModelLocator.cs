878557b3c4268d60fa54d232cd5568b0467db2fb
MySky20164407 <215301160@qq.com>
2019-06-18 16:51:41 +0800

    master

---
+++ FileSys/Locator/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+
+namespace FileSys.Locator {
+    class ViewModelLocator {
+        /// <summary>
+        ///     ViewModel定位器单件
+        /// </summary>
+        public static readonly ViewModelLocator Instance = new ViewModelLocator();
+
+        /// <summary>
+        ///     私有构造
+        /// </summary>
+        private ViewModelLocator() {        
+        }
+
+    }
+}
