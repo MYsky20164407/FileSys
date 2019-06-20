c62cd864efe759338963fa6b121dc6b21e88f395
MySky20164407 <215301160@qq.com>
2019-06-20 15:17:20 +0800

    interface prototype

--- FileSysLib/IServices/ICoreService.cs
+++ FileSysLib/IServices/ICoreService.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace FileSysLib.IServices {
+    /// <summary>
+    /// 文件系统内核，提供系统调用服务
+    /// </summary>
     interface ICoreService {
+        void CreatFile();
+        void OpenFile();
+        void ReadFile();
+        void WriteFile();
+        void CloseFIle();
+        void DeleteFile();
+        void MakeDir();
+        void OpenDir();
+
+        void Select();
     }
 }
--- FileSysLib/IServices/IDiskManagementService.cs
+++ FileSysLib/IServices/IDiskManagementService.cs
@@ -3,6 +3,28 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace FileSysLib.IServices {
+    /// <summary>
+    /// 管理空闲盘块和空闲i节点，负责盘块与i节点的分配与回收
+    /// </summary>
     interface IDiskManagementService {
+        /// <summary>
+        /// 申请空闲盘块
+        /// </summary>
+        void GetFreeBlock();
+
+        /// <summary>
+        /// 申请空闲i节点
+        /// </summary>
+        void GetFreeInode();
+
+        /// <summary>
+        /// 回收盘块
+        /// </summary>
+        void RetrieveBlock(int index);
+
+        /// <summary>
+        /// 回收i节点
+        /// </summary>
+        void RetrieveInode(int index);
     }
 }
